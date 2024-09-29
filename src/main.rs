5709c0ded4a8dcf9f47858c3741aea3a4544ab6a
aidengage <aidenfgage@gmail.com>
2024-09-29 13:55:28 -0400

    added check for file existing and created move function

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,22 @@ use std::io;
 use std::path::Path;
 use std::string::ToString;
 
-// fn check_file() {
-//
-// }
 const UPLOAD_DIR: &str = "/Users/aidengage/dev/senior/cate/file-uploaded/";
+fn check_file(file_path: &str) -> bool {
+    if let Ok(file) = File::open(file_path) {
+        true
+    } else {
+        false
+    }
+}
+
+// fn check_file(file_path: &str) {
+//     let file_path_result = Path::new(file_path).try_exists();
+//     let checked_path = match file_path_result {
+//         Ok(path) => path,
+//         Err(error) => panic!("{}: file does not exist", error),
+//     };
+// }
 
 fn dir_to_vec(file_path: String) -> Vec<u8> {
     let clean_path: String = file_path.clone().trim().to_string();
@@ -29,7 +41,7 @@ fn vec_to_file(vec: Vec<u8>, file_name: String) {
     }
 }
 
-fn file_name(file_path: &String) -> String {
+fn get_file_name(file_path: &String) -> String {
     let mut file_name = String::new();
     let mut reverse_file_name = String::new();
     let reverse_path = file_path.chars().rev().collect::<String>();
@@ -44,13 +56,29 @@ fn file_name(file_path: &String) -> String {
     file_name
 }
 
-fn main() {
-    let path = "/Users/aidengage/dev/senior/cate/file-for-upload/fabric-api-0.103.0+1.21.1.jar".to_string();
-    let file_name = file_name(&path);
-    println!("File name: {}", file_name);
+fn move_file(file_path: &String) {
+    if (check_file(file_path)) {
+        let file_vector = dir_to_vec(file_path.to_string());
+        let file = vec_to_file(file_vector, get_file_name(file_path));
+    } else {
+        println!("File does not exist");
+    }
+}
 
-    let file_data = dir_to_vec(path);
-    vec_to_file(file_data, file_name);
+fn main() {
+    // println!("{}", check_file("/Users/aidengage/dev/senior/cate/file-for-upload/fabric-api-0.103.0+1.21.1.jar"));
+    // println!("{}", check_file("/Users/aidengage/dev/senior/cate/file-for-upload/whatiasdohe.txt"));
+    // check_file("/Users/aidengage/dev/senior/cate/file-for-upload/fabric-api-0.103.0+1.21.1.jar");
+    // check_file("/Users/aidengage/dev/senior/cate/file-for-upload/whatiasdohe.txt");
+
+    let path = "/Users/aidengage/dev/senior/cate/file-for-upload/fabric-api-0.103.0+1.2.jar".to_string();
+    move_file(&path);
+
+    // let file_name = get_file_name(&path);
+    // println!("File name: {}", file_name);
+    // let file_data = dir_to_vec(path);
+    // vec_to_file(file_data, file_name);
+
     // let mut file = File::create(UPLOAD_DIR.to_string() + file_name.as_str()).unwrap();
     // file.write_all(&file_data).unwrap();
 }
