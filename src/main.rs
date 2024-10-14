4d21aef078e1f8e7884e4f9870254a15b8e990e5
aidengage <aidenfgage@gmail.com>
2024-10-13 21:05:40 -0400

    receive file name, should be in checkpoint 2

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 mod receiver;
 
-use std::fs;
-use std::fs::File;
 use std::io::{Read, Write};
-use std::path::Path;
 use std::string::ToString;
-use std::thread;
 use std::str;
-use std::net::{Ipv4Addr, SocketAddrV4};
-use std::net::{TcpListener, TcpStream, Shutdown};
+use std::net::{Ipv4Addr};
 
-// const UPLOAD_DIR: &str = "/Users/aidengage/dev/senior/cate/file-uploaded/";
-const UPLOAD_DIR: &str = "/home/cate/cate/upload/";
-// const ADDR: Ipv4Addr = Ipv4Addr::LOCALHOST;
-const ADDR: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 104);
+const UPLOAD_DIR: &str = "/Users/aidengage/dev/senior/cate/file-uploaded/";
+// const UPLOAD_DIR: &str = "/home/cate/cate/upload/";
+const ADDR: Ipv4Addr = Ipv4Addr::LOCALHOST;
+// const ADDR: Ipv4Addr = Ipv4Addr::new(192, 168, 1, 104);
+
 const PORT: u16 = 8000;
 
 fn main() {
--- src/receiver.rs
+++ src/receiver.rs
@@ -11,7 +11,8 @@ fn vec_to_file(vec: Vec<u8>, file_name: String) {
     } else {
         // let mut file = File::create(UPLOAD_DIR.to_string() + file_name.as_str()).unwrap();
         // let mut file = File::create(UPLOAD_DIR.to_string() + "file.cpp").unwrap();
-        let mut file = File::create(UPLOAD_DIR.to_string() + "cate2.zip").unwrap();
+        // let mut file = File::create(UPLOAD_DIR.to_string() + "cate2.zip").unwrap();
+        let mut file = File::create(UPLOAD_DIR.to_string() + &file_name).unwrap();
         // let mut file = File::create(UPLOAD_DIR.to_string() + "temp.txt").unwrap();
         // let mut file = File::create(UPLOAD_DIR.to_string() + "newcpp.txt").unwrap();
         // let mut file = File::create(UPLOAD_DIR.to_string() + "fabric-api.jar").unwrap();
@@ -37,37 +38,35 @@ pub fn receive_file(/*mut file: &Vec<u8>*/) {
     }
 }
 
-
 pub fn handle_client(mut stream: TcpStream, /* size: u64 */ ) {
+
+    let mut name_length_buffer = [0u8; 8];
+    stream.read_exact(&mut name_length_buffer).unwrap();
+    println!("name buffer: {:?}", name_length_buffer);
+    let name_len = u64::from_be_bytes(name_length_buffer);
+
+    let mut name_buffer = vec![0u8; name_len as usize];
+    stream.read_exact(&mut name_buffer).unwrap();
+    let file_name = String::from_utf8(name_buffer).unwrap();
+    println!("name: {}", file_name);
+
     let mut length_buffer = vec![0; 8];
-    loop {
+    // loop {
         if let Err(error) = stream.read_exact(&mut length_buffer) {
             println!("failed to read length: {}", error);
-            break;
+            // break;
         }
         let length: Vec<u8> = length_buffer.to_vec();
         // let file_size: u64 = u64::from_be_bytes(length_buffer.try_into().unwrap());
         // println!("file size: {}", file_size);
         // println!("size var: {}", size);
         println!("length array: {:?}", length);
-        break;
-    }
+        // break;
+    // }
 
     // let mut buffer = [0; 1024];
     let mut buffer = Vec::new();
     let mut temp_buffer = [0; 1024];
-    let mut received_bytes: Vec<u8> = Vec::new();
-
-
-    // loop {
-    //     let bytes_read = stream.read(&mut temp_buffer).unwrap();
-    //     if bytes_read == 0 {
-    //         break;
-    //     }
-    //     buffer.extend_from_slice(&temp_buffer[0..bytes_read]);
-    // }
-
-    let mut bytes_read = 0;
 
     println!("received:");
     loop {
@@ -83,12 +82,9 @@ pub fn handle_client(mut stream: TcpStream, /* size: u64 */ ) {
 
         // Process the chunk of data (for demonstration, we append to the data vector)
         buffer.extend_from_slice(&temp_buffer[..bytes_read]);
-
-        // println!("Received {} bytes", bytes_read);
-        // Optionally, you can print the data chunk received (e.g., if it's text)
-        // println!("Chunk: {}", String::from_utf8_lossy(&buffer[..bytes_read]));
     }
 
     println!("buffer size {}", buffer.len());
-    vec_to_file(buffer, UPLOAD_DIR.to_string());
+    // vec_to_file(buffer, UPLOAD_DIR.to_string());
+    vec_to_file(buffer, file_name.to_string());
 }
