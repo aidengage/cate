6044a8aba6837f7ca11a4be146cfa217963b8445
aidengage <aidenfgage@gmail.com>
2024-10-27 14:24:48 -0400

    seperated sender and ui

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
-=======
-// mod filedrop;
-// slint?::include_modules!();
+mod sender;
 
-// use slint;
-//
-// use winit::{
-//     event::{Event, WindowEvent},
-//     event_loop::{ControlFlow, EventLoop},
-//     // platform::macos::WindowExtMacOS,
-//     window::Window,
-// };
-// use winit::platform::run_on_demand::EventLoopExtRunOnDemand;
-
->>>>>>> 862c09c3d706b4d54a13a8edb863312701dd32ab
 use std::error::Error;
 use std::fs;
 use std::fs::File;
@@ -34,23 +20,23 @@ const PORT: u16 = 8000;
 
 
 
-fn check_file(file_path: &str) -> bool {
-    if let Ok(_file) = File::open(file_path) {
-        true
-    } else {
-        false
-    }
-}
+// fn check_file(file_path: &str) -> bool {
+//     if let Ok(_file) = File::open(file_path) {
+//         true
+//     } else {
+//         false
+//     }
+// }
 
-fn dir_to_vec(file_path: String) -> Vec<u8> {
-    let clean_path: String = file_path.clone().trim().to_string();
-    if Path::new(clean_path.as_str()).exists() {
-        let file_contents: Vec<u8> = fs::read(clean_path).unwrap();
-        file_contents
-    } else {
-        Vec::new()
-    }
-}
+// fn dir_to_vec(file_path: String) -> Vec<u8> {
+//     let clean_path: String = file_path.clone().trim().to_string();
+//     if Path::new(clean_path.as_str()).exists() {
+//         let file_contents: Vec<u8> = fs::read(clean_path).unwrap();
+//         file_contents
+//     } else {
+//         Vec::new()
+//     }
+// }
 
 // fn vec_to_file(vec: Vec<u8>, file_name: String) {
 //     if vec.len() == 0 {
@@ -61,177 +47,140 @@ fn dir_to_vec(file_path: String) -> Vec<u8> {
 //     }
 // }
 
-fn vec_to_discard(vec: Vec<u8>, file_name: String) {
-    if vec.len() == 0 {
-        return;
-    } else {
-        // let mut file = File::create(UPLOAD_DIR.to_string() + file_name.as_str()).unwrap();
-        // let mut file = File::create("/Users/aidengage/dev/senior/cate/discard/".to_string() + file_name.as_str()).unwrap();
-        let mut file = File::create(DISCARD.to_string() + file_name.as_str()).unwrap();
-        file.write_all(&vec).unwrap();
-    }
-}
+// fn vec_to_discard(vec: Vec<u8>, file_name: String) {
+//     if vec.len() == 0 {
+//         return;
+//     } else {
+//         // let mut file = File::create(UPLOAD_DIR.to_string() + file_name.as_str()).unwrap();
+//         // let mut file = File::create("/Users/aidengage/dev/senior/cate/discard/".to_string() + file_name.as_str()).unwrap();
+//         let mut file = File::create(DISCARD.to_string() + file_name.as_str()).unwrap();
+//         file.write_all(&vec).unwrap();
+//     }
+// }
 
-fn remove_file(file_path: String) {
-    fs::remove_file(file_path).unwrap();
-}
+// fn remove_file(file_path: String) {
+//     fs::remove_file(file_path).unwrap();
+// }
 
-fn get_file_name(file_path: &String) -> String {
-    let mut reverse_file_name = String::new();
-
-    let reverse_path = file_path.chars().rev().collect::<String>();
-    for c in reverse_path.chars() {
-        if c != '/' {
-            reverse_file_name.push(c);
-        } else {
-            break;
-        }
-    }
-    let file_name = reverse_file_name.chars().rev().collect::<String>();
-    file_name
-}
+// fn get_file_name(file_path: &String) -> String {
+//     let mut reverse_file_name = String::new();
+//
+//     let reverse_path = file_path.chars().rev().collect::<String>();
+//     for c in reverse_path.chars() {
+//         if c != '/' {
+//             reverse_file_name.push(c);
+//         } else {
+//             break;
+//         }
+//     }
+//     let file_name = reverse_file_name.chars().rev().collect::<String>();
+//     file_name
+// }
 
 // hmmmmm
-fn move_file(file_path: String) {
-    if check_file(&file_path) {
-        let file_vector = dir_to_vec(file_path.to_string());
-        // vec_to_file(file_vector, get_file_name(file_path));
-        vec_to_discard(file_vector, file_path.to_string());
-    } else {
-        println!("File does not exist");
-    }
-}
+// fn move_file(file_path: String) {
+//     if check_file(&file_path) {
+//         let file_vector = dir_to_vec(file_path.to_string());
+//         // vec_to_file(file_vector, get_file_name(file_path));
+//         vec_to_discard(file_vector, file_path.to_string());
+//     } else {
+//         println!("File does not exist");
+//     }
+// }
 
-fn send_file() -> std::io::Result<()> {
-    println!("Hello Client!");
-    // let mut progress_var = 0;
-
-    // let paths = fs::read_dir("/Users/aidengage/dev/senior/cate/file-for-upload/")?;
-    let paths = fs::read_dir(PULL_DIR)?;
-    // let mut directory = String::new();
-    // let mut file_name = String::new();
-    for path in paths {
-        let directory = path?.path().display().to_string();
-        let file_name = get_file_name(&directory);
-        // file_name = get_file_name(&path?.path().display().to_string());
-        if file_name.as_bytes()[0] as char != '.' {
-            // println!("Path: {}", directory);
-
-            // }
-            let name_of_file = file_name.clone();
-            if let Ok(mut stream) = TcpStream::connect(SocketAddrV4::new(ADDR, PORT)) {
-                println!("Connected to the server on {:?}", stream.peer_addr()?);
-
-                let full_path = PULL_DIR.to_string() + name_of_file.as_str();
-
-                let mut file_size = 0;
-                match metadata(&full_path) {
-                    Ok(metadata) => {
-                        file_size = metadata.len();
-                        // if file_size > isize::MAX as u64 {
-                        //     println!("one of your files is too large (over {})", isize::MAX as u64);
-                        //     // shutdown causes issue when sending nothing
-                        //     stream.shutdown(Shutdown::Both).expect("shutdown call failed");
-                        //     continue;
-                        // }
-                        println!("File size: {}", file_size);
-                    }
-                    Err(error) => {
-                        println!("Error: {}", error);
-                    }
-                }
-
-                let name_vec = file_name.into_bytes();
-                let name_len = name_vec.len().to_be_bytes().to_vec();
-
-                stream.write(&name_len)?;
-                // println!("name vec: {:?}", name_vec);
-                stream.write_all(&name_vec)?;
-
-
-
-                let message = dir_to_vec(full_path.clone());
-
-                // let length = vec![message.len() as u8];
-                let size_array = file_size.to_be_bytes().to_vec();
-
-                // println!("size array: {:?}", size_array);
-
-                stream.write(&size_array)?;
-
-                match message[..] {
-
-                    // "#END#" => stream.shutdown(Shutdown::Both).expect("Shutdown Failed!"),
-                    [] => stream.shutdown(Shutdown::Both).expect("shutdown call failed"),
-                    _ => {
-                        println!("SENT!");
-                        stream.write(&message)?;
-
-                        // let mut progress_buffer = [0;1];
-                        // let bool = if progress_buffer[0] == 1 { true } else { false };
-                        // println!("{:?}", bool);
-                        // println!("progress buffer: {:?}", progress_buffer);
-                        // loop {
-                        //     if stream.read(&mut progress_buffer)? == 0 {
-                        //         println!("progress in bytes: {}", progress_var);
-                        //         break;
-                        //     } else {
-                        //         progress_var += 1;
-                        //         print!("|");
-                        //     }
-                        // }
-
-                        vec_to_discard(message, name_of_file.clone());
-                        remove_file(full_path.to_string());
-                        // move_file(DISCARD.to_string() + file_name.as_str());
-                    }
-                }
-            } else {
-                println!("Couldn't connect to server...");
-            }
-        }
-    }
-
-    Ok(())
-}
+// fn send_file() -> std::io::Result<()> {
+//     println!("Hello Client!");
+//     // let mut progress_var = 0;
+//
+//     // let paths = fs::read_dir("/Users/aidengage/dev/senior/cate/file-for-upload/")?;
+//     let paths = fs::read_dir(PULL_DIR)?;
+//     // let mut directory = String::new();
+//     // let mut file_name = String::new();
+//     for path in paths {
+//         let directory = path?.path().display().to_string();
+//         let file_name = sender::get_file_name(&directory);
+//         // file_name = get_file_name(&path?.path().display().to_string());
+//         if file_name.as_bytes()[0] as char != '.' {
+//             // println!("Path: {}", directory);
+//
+//             // }
+//             let name_of_file = file_name.clone();
+//             if let Ok(mut stream) = TcpStream::connect(SocketAddrV4::new(ADDR, PORT)) {
+//                 println!("Connected to the server on {:?}", stream.peer_addr()?);
+//
+//                 let full_path = PULL_DIR.to_string() + name_of_file.as_str();
+//
+//                 let mut file_size = 0;
+//                 match metadata(&full_path) {
+//                     Ok(metadata) => {
+//                         file_size = metadata.len();
+//                         // if file_size > isize::MAX as u64 {
+//                         //     println!("one of your files is too large (over {})", isize::MAX as u64);
+//                         //     // shutdown causes issue when sending nothing
+//                         //     stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+//                         //     continue;
+//                         // }
+//                         println!("File size: {}", file_size);
+//                     }
+//                     Err(error) => {
+//                         println!("Error: {}", error);
+//                     }
+//                 }
+//
+//                 let name_vec = file_name.into_bytes();
+//                 let name_len = name_vec.len().to_be_bytes().to_vec();
+//
+//                 stream.write(&name_len)?;
+//                 // println!("name vec: {:?}", name_vec);
+//                 stream.write_all(&name_vec)?;
+//
+//
+//
+//                 let message = dir_to_vec(full_path.clone());
+//
+//                 // let length = vec![message.len() as u8];
+//                 let size_array = file_size.to_be_bytes().to_vec();
+//
+//                 // println!("size array: {:?}", size_array);
+//
+//                 stream.write(&size_array)?;
+//
+//                 match message[..] {
+//
+//                     // "#END#" => stream.shutdown(Shutdown::Both).expect("Shutdown Failed!"),
+//                     [] => stream.shutdown(Shutdown::Both).expect("shutdown call failed"),
+//                     _ => {
+//                         println!("SENT!");
+//                         stream.write(&message)?;
+//
+//                         // let mut progress_buffer = [0;1];
+//                         // let bool = if progress_buffer[0] == 1 { true } else { false };
+//                         // println!("{:?}", bool);
+//                         // println!("progress buffer: {:?}", progress_buffer);
+//                         // loop {
+//                         //     if stream.read(&mut progress_buffer)? == 0 {
+//                         //         println!("progress in bytes: {}", progress_var);
+//                         //         break;
+//                         //     } else {
+//                         //         progress_var += 1;
+//                         //         print!("|");
+//                         //     }
+//                         // }
+//
+//                         vec_to_discard(message, name_of_file.clone());
+//                         remove_file(full_path.to_string());
+//                         // move_file(DISCARD.to_string() + file_name.as_str());
+//                     }
+//                 }
+//             } else {
+//                 println!("Couldn't connect to server...");
+//             }
+//         }
+//     }
+//
+//     Ok(())
+// }
 
 fn main() {
-
-<<<<<<< HEAD
-=======
-    println!("max size: {}", isize::MAX as usize);
-
-    // let ui = AppWindow::new()?;
-
-    //
-    // let event_loop = EventLoop::new();
-    // let window = Window::default_attributes();
-
-    //
-
-    // println!("window size: {:?}", ui.window().size());
-
-    //
-    // event_loop.unwrap().run_app(&mut move |event, _, control_flow: ControlFlow| {
-    //     *control_flow = ControlFlow::Wait;
-    //
-    //     match event {
-    //         Event::WindowEvent { event: WindowEvent::DroppedFile(path), .. } => {
-    //             println!("file path: {:?}", path);
-    //         }
-    //         _ => (),
-    //     }
-    // }).expect("this panic message means something idk");
-    //
-
-    // ui.on_request_send_file({
-    //     move || {
-    //         send_file().unwrap();
-    //     }
-    // });
-
-    // ui.run()?;
-
-    Ok(())
->>>>>>> 862c09c3d706b4d54a13a8edb863312701dd32ab
+    sender::send_file().unwrap();
 }
