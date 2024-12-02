d576a1b12f55296964ead4ce2cd852a585d452f3
aidengage <aidenfgage@gmail.com>
2024-12-01 21:06:37 -0500

    remove project root dir

--- src/server/main.rs
+++ src/server/main.rs
@@ -5,11 +5,6 @@ use std::net::{Ipv4Addr};
 use std::path::Path;
 use nix::unistd::Uid;
 
-const ROOT_DIR: String = env::var("PROJECT_ROOT").unwrap_or_else(|_| env::current_dir().unwrap().to_str().unwrap().to_string());
-// println!("project_root: {}", project_root);
-// let file_path = Path::new(&project_root).join("assets/links.txt");
-// println!("file_path: {:?}", file_path);
-
 // const UPLOAD_DIR: &str = "/Users/aidengage/dev/senior/cate/server-upload/";
 const UPLOAD_DIR: &str = "/var/www/html/files/";
 // const UPLOAD_DIR: &str = "/home/cate/cate/upload/";
